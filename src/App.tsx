80cd47668f5dcd2369fa5f964df24f16965f9ccb
Rodrigo Marquez <a@Rodrigo-Marquez-MacBook-Air.local>
2025-06-05 18:01:39 -0300

    Se destructura el value y el map y el filter se pasan a funciones

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,27 @@ function App() {
   const names = ["Ana", "Bruno", "Carla", "Diego"];
   const [filteredNames, setFilteredNames] = useState(names);
 
+  function renderNames () {
+    return filteredNames.map((name: string, index: number) => (
+      <li key={index}>{name}</li>
+    ))
+  }
+
+  function filterNames () {
+    return <input type="text" placeholder="Escribe un nombre:" onChange={(e) => {
+      const { value } = e.target;
+      setFilteredNames(names.filter(name => name.toLowerCase().includes(value.toLowerCase())))
+    }} />
+  }
+
   return (           
     <>
       <h2>Lista de nombres:</h2>
       <ul>
-        {filteredNames.map((name: string, index: number) => (
-          <li key={index}>{name}</li>
-        ))}
+        {renderNames()}
       </ul>
       <h3>Filtrar nombres:</h3>
-      <input type="text" placeholder="Escribe un nombre:" onChange={(e) => {
-        const value = e.target.value;
-        setFilteredNames(names.filter(name => name.toLowerCase().includes(value.toLowerCase())))
-      }} />
+      {filterNames()}
     </>
   )
 }
